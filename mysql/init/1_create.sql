94f8187ac4f585f914ecf1d9641cc1105a20d0d5
hikotomo <tomo.k9090@gmail.com>
2021-09-09 20:30:40 +0900

    file modify

--- mysql/init/1_create.sql
+++ mysql/init/1_create.sql
@@ -1,16 +1,35 @@
--- -- golang_dbという名前のデータベースを作成
--- CREATE DATABASE golang_db;
--- -- golang_dbをアクティブ
--- use golang_db;
--- -- usersテーブルを作成。名前とパスワード
--- CREATE TABLE users (
---     id INT(11) AUTO_INCREMENT NOT NULL,
---     name VARCHAR(64) NOT NULL,
---     password CHAR(30) NOT NULL,
---     PRIMARY KEY (id)
--- );
--- -- usersテーブルに２つレコードを追加
--- INSERT INTO users (name, password) VALUES ("gophar", "5555");
--- INSERT INTO users (name, password) VALUES ("octcat", "0000");
+CREATE DATABASE capsule;
 
+use capsule;
 
+CREATE TABLE User
+(
+    id INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
+    name varchar(10) NOT NULL UNIQUE,
+    token varchar(10) NOT NULL UNIQUE
+);
+
+CREATE TABLE Characters
+(
+    id INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
+    name varchar(20) NOT NULL UNIQUE,
+    rarity varchar(20) NOT NULL,
+    attack INT NOT NULL,
+    defence INT NOT NULL,
+    recovery INT NOT NULL
+);
+
+CREATE TABLE UserCharacters
+(
+    id INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    character_id INT NOT NULL
+);
+
+INSERT INTO Characters(name, rarity, attack, defence, recovery) VALUES("ミシシッピアカミミガメ", "☆", 3,3,0);
+INSERT INTO Characters(name, rarity, attack, defence, recovery) VALUES("イシガメ", "☆☆", 1,2,4);
+INSERT INTO Characters(name, rarity, attack, defence, recovery) VALUES("スッポン", "☆☆☆", 5,1,2);
+INSERT INTO Characters(name, rarity, attack, defence, recovery) VALUES("クサガメ", "☆☆☆☆", 2,4,3);
+INSERT INTO Characters(name, rarity, attack, defence, recovery) VALUES("カミツキガメ", "☆☆☆☆☆", 7,5,1);
+INSERT INTO Characters(name, rarity, attack, defence, recovery) VALUES("リュウキュウヤマガメ", "☆☆☆☆☆☆", 4,4,7);
+INSERT INTO Characters(name, rarity, attack, defence, recovery) VALUES("ウミガメ", "☆☆☆☆☆☆☆", 6,6,6);
